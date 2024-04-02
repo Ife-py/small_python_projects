c990c7a9fbf25e8c864ee049b837be82cf891fcd
Ifeoluwa <omolekeifeoluwa2005@gmail.com>
2024-04-02 13:46:57 +0100

    update bagels.py

--- bagels.py
+++ bagels.py
@@ -2,13 +2,13 @@
 
 import random
 
-Num_Digits=3
-Max_Guesses = 10
+NUM_DIGITS=3
+MAX_GUESSES = 10
 
 def main():
     print(f"""'Bagels, a deductive logic game.
      By Al Sweigart al@inventwithpython.com
-    I am thinking of a {Num_Digits}-digit number with no repeated digits.
+    I am thinking of a {NUM_DIGITS}-digit number with no repeated digits.
     Try to guess what it is. Here are some clues:
     When I say:    That means:
     Pico         One digit is correct but in the wrong position.
@@ -17,28 +17,28 @@ def main():
     For example, if the secret number was 248 and your guess was 843, the
     clues would be Fermi Pico""")
     while True:
-        secretNum= get_secret_num()
+        secret_Num= get_secret_num()
         print('I have thought of a number.')
-        print(f'You have {Max_Guesses} guesses to get it.')
+        print(f'You have {MAX_GUESSES} guesses to get it.')
     
         num_guesses =1
-        while num_guesses <=Max_Guesses:
+        while num_guesses <=MAX_GUESSES:
             guess=''
             #keeps loop going until they enter a valid guess:
-            while len(guess) != Num_Digits or not guess.isdecimal():
+            while len(guess) != NUM_DIGITS or not guess.isdecimal():
                 print(f'Guess #{num_guesses}:')
                 guess=input('>')
 
-                clues=get_clues(guess, secretNum)
+                clues=get_clues(guess, secret_Num)
                 print(clues)
                 num_guesses +=1
 
-                if guess==secretNum:
+                if guess==secret_Num:
                     return'you guessed correctly' 
                     break #if their guess is correct it breaks out of the loop here.
-                if num_guesses > Max_Guesses:
+                if num_guesses > MAX_GUESSES:
                     print('You ran out of guesses.')
-                    print(f'The answer was {secretNum}')
+                    print(f'The answer was {secret_Num}')
 
         #Ask if the player wants to play again.
         print('Do you want to play again?(yes or no)')
@@ -54,20 +54,20 @@ def get_secret_num():
     #Get the first digits in the list for the secret number
 
     secretNum=''
-    for i in range(Num_Digits):
+    for i in range(NUM_DIGITS):
         secretNum += str(numbers[i])
     return secretNum
 
-def get_clues(guess, secreNum):
-    if guess== secreNum:
+def get_clues(guess, secret_Num):
+    if guess== secret_Num:
         return'You got it!'
     
     clues=[]
 
     for i in range(len(guess)):
-        if guess[i]==secreNum[i]:
+        if guess[i]==secret_Num[i]:
             clues.append('Fermi')
-        elif guess[i] in secreNum:
+        elif guess[i] in secret_Num:
             clues.append('Pico')
 
     if len(clues)== 0:
